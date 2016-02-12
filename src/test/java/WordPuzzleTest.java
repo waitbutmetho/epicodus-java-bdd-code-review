210165ca104827ac59ee167744cd8a6d6b0d522b
Josh <silversevenfox@yahoo.com>
2016-02-12 08:09:13 -0800

    add first spec for a to -

--- src/test/java/WordPuzzleTest.java
+++ src/test/java/WordPuzzleTest.java
@@ -4,5 +4,9 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 public class WordPuzzleTest {
-
+  @Test
+  public void stringToWordPuzzle_Replace_a_WithDash_4() {
+    WordPuzzle testWordPuzzle = new WordPuzzle();
+    assertEquals("l-l", testWordPuzzle.stringToWordPuzzle("lal"));
+  }
 }

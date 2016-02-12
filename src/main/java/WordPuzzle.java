0fb02ce38d2c2fb00fc8961c8bcaec5b5228f649
Josh <silversevenfox@yahoo.com>
2016-02-12 08:24:42 -0800

    add functionality for u to -

--- src/main/java/WordPuzzle.java
+++ src/main/java/WordPuzzle.java
@@ -7,7 +7,7 @@ public class WordPuzzle {
   public static void main(String[] args) {}
 
     public static String stringToWordPuzzle(String userString) {
-  String wordPuzzleString = userString.replaceAll("[aeio]", "-");
+  String wordPuzzleString = userString.replaceAll("[aeiou]", "-");
   return wordPuzzleString;
   }
 }

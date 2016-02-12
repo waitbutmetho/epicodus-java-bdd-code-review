ff3f1d57c1b60972fb497d940bc0874b3fcdc343
Josh <silversevenfox@yahoo.com>
2016-02-12 08:31:33 -0800

    setup spark server w/ templates

--- src/main/java/WordPuzzle.java
+++ src/main/java/WordPuzzle.java
@@ -4,7 +4,28 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class WordPuzzle {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+  String layout = "templates/layout.vtl";
+  get("/", (request, response) -> {
+    HashMap model = new HashMap();
+
+  model.put("template", "templates/index.vtl");
+  return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+
+  get("/results", (request, response) -> {
+    HashMap model = new HashMap();
+    String userString = request.queryParams("stringInput");
+    String userWordPuzzle = stringToWordPuzzle(userString);
+
+    model.put("userWordPuzzle", userWordPuzzle);
+
+    model.put("template", "templates/results.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+  }
 
     public static String stringToWordPuzzle(String userString) {
   String wordPuzzleString = userString.replaceAll("[aeiouAEIOU]", "-");

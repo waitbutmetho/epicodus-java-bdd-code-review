5b3286a18a1114ad6c149ade597a18a44307c3e4
Josh <silversevenfox@yahoo.com>
2016-02-12 08:21:37 -0800

    add functionality for i to -

--- src/main/java/WordPuzzle.java
+++ src/main/java/WordPuzzle.java
@@ -7,7 +7,7 @@ public class WordPuzzle {
   public static void main(String[] args) {}
 
     public static String stringToWordPuzzle(String userString) {
-  String wordPuzzleString = userString.replaceAll("[ae]", "-");
+  String wordPuzzleString = userString.replaceAll("[aei]", "-");
   return wordPuzzleString;
   }
 }

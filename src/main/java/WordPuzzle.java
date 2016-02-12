9dc8052e99648f9f8245c7b21109117128ec4a83
Josh <silversevenfox@yahoo.com>
2016-02-12 08:07:38 -0800

    initial commit

---
+++ src/main/java/WordPuzzle.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class WordPuzzle {
+  public static void main(String[] args) {}
+}
